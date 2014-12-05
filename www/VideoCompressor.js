046378e34eb6e180f52b0a54bcdc35e0e0133c9f
AsmitaAK <asmita.koli@gmail.com>
2014-12-05 16:11:40 +0530

    updated .js

--- www/VideoCompressor.js
+++ www/VideoCompressor.js
@@ -1,33 +1,7 @@
-// ActionScript Communications Document
-var VideoCompressor = function() {
-};
-
-
-// Call this to register for push notifications. Content of [options] depends on whether we are working with APNS (iOS) or GCM (Android)
-VideoCompressor.start = function(successCallback, errorCallback, options) {
-    if (errorCallback == null) { errorCallback = function() {}}
-
-    if (typeof errorCallback != "function")  {
-        console.log("VideoCompressor.register failure: failure parameter not a function");
-        return
+var exec = require("cordova/exec");
+module.exports = {
+	 start: function(success, failure) {
+       exec(successCallback, errorCallback, "VideoCompressor", "start", []);
     }
-
-    if (typeof successCallback != "function") {
-        console.log("VideoCompressor.register failure: success callback parameter must be a function");
-        return
-    }
-
-    cordova.exec(successCallback, errorCallback, "VideoCompressor", "start", [options]);
+	
 };
-
-
-VideoCompressor.install = function () {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
-
-  window.plugins.videoCompressor = new VideoCompressor();
-  return window.plugins.videoCompressor;
-};
-
-cordova.addConstructor(VideoCompressor.install);
